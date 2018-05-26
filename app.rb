de4c54e01f28932f17dbc525b91f66840677e512
Joseph Parker <jparker010190@gmail.com>
2018-05-26 18:33:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ class App < Sinatra::Base
     erb :team
   end
   post '/team' do
-    "Team Name: #{params[:name]}"
-    "Coach: #{params[:coach]}"
-    "Point Guard: #{params[:pg]}"
-    "Shooting Guard: #{params[:sg]}"
-    "Small Forward: #{params[:sf]}"
-    "Power Forward: #{params[:pf]}"
-    "Center: #{params[:c]}"
+    "Team Name: #{params[:name]}
+    Coach: #{params[:coach]}
+    Point Guard: #{params[:pg]}
+    Shooting Guard: #{params[:sg]}
+    Small Forward: #{params[:sf]}
+    Power Forward: #{params[:pf]}
+    Center: #{params[:c]}"
   end
 end
