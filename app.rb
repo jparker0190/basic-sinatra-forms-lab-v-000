b6c558662a61e6af1080fd46da986359acf21dba
Joseph Parker <jparker010190@gmail.com>
2018-05-26 18:31:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,5 +9,11 @@ class App < Sinatra::Base
   end
   post '/team' do
     "Team Name: #{params[:name]}"
+    "Team Name: #{params[:coach]}"
+    "Team Name: #{params[:pg]}"
+    "Team Name: #{params[:sg]}"
+    "Small Forward: #{params[:sf]}"
+    "Power Forward: #{params[:pf]}"
+    "Center: #{params[:c]}"
   end
 end
