58b744933c025a1e55aef3d1deb3da5bfa40e152
Joseph Parker <jparker010190@gmail.com>
2018-05-26 18:29:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-
+  get '/newteam' do
+    erb :newteam
+  end
+  get '/team' do
+    erb :team
+  end
+  post '/team' do
+    "Team Name: #{params[:name]}"
+  end
 end
